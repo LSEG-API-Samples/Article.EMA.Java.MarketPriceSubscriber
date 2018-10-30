2a30c7f68dfac1a4a1617f4452d6871899f48cc3
Jirapongse Phuriphanvichai <jirapongse.phuriphanvichai@thomsonreuters.com>
2018-10-30 13:05:41 +0700

    Add files via upload
    Build Script for EMA 1.2.1

---
+++ build321.bat
@@ -0,0 +1,51 @@
+@echo off
+setlocal
+
+rem
+rem The following batch file assumes the following environment:
+rem 
+rem		JAVA_HOME - Root directory of your JDK 8 environment
+rem 	ELEKTRON_JAVA_HOME - Root directory of your (EMA) Elektron Java API installation
+rem		
+
+set SCRIPT=%0
+set BINDIR=build\classes
+set DISTDIR=dist
+set VALUE_ADD_OBJECTS_FOR_EMA_HOME=./ValueAddObjectsForEMA
+
+set JAVAC="%JAVA_HOME%\bin\javac"
+set JAR="%JAVA_HOME%\bin\jar"
+
+set CLASSPATH=%BINDIR%;%VALUE_ADD_OBJECTS_FOR_EMA_HOME%/dist/ValueAddObjectsForEMA.jar;lib/commons-cli-1.4.jar;lib/json-20160810.jar
+
+if not exist %VALUE_ADD_OBJECTS_FOR_EMA_HOME% goto :NO_SUBMODULE
+if not exist %VALUE_ADD_OBJECTS_FOR_EMA_HOME%\build.bat goto :NO_SUBMODULE
+
+cd ValueAddObjectsForEMA
+call build321.bat
+cd ..
+
+if not exist %BINDIR% (mkdir %BINDIR%)
+if not exist %DISTDIR% (mkdir %DISTDIR%)
+echo Building the ChainExpander application...
+%JAVAC% -Xlint -d %BINDIR% src\com\thomsonreuters\platformservices\elektron\tools\chain\*.java
+if %errorlevel% neq 0 goto :ERROR
+
+echo Building the jar file...
+%JAR% cfm %DISTDIR%\ChainExpander.jar Manifest.txt -C build\classes .
+if %errorlevel% neq 0 goto :ERROR
+
+goto :DONE
+
+:NO_SUBMODULE
+echo.
+echo The %VALUE_ADD_OBJECTS_FOR_EMA_HOME% submodule is missing or incomplete.
+goto :ERROR
+
+:ERROR
+echo.
+echo Build failed.  Exiting.
+goto :EOF
+
+:DONE
+echo ** ChainExpander project successfully built **
